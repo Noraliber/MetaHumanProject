84fbd993462dc794098fc4514fb3f14425286df1
Noraliber <108892844+Noraliber@users.noreply.github.com>
2023-10-23 15:20:16 +0000

    Update main.py

--- main.py
+++ main.py
@@ -16,8 +16,9 @@ def index():
 
 @app.route("/transcribe", methods=['POST'])
 def transcribe():
-    audio_file= open("/path/to/file/audio.mp3", "rb")
-
+    audio_file= open("/workspace/Enregistrement.m4a", "rb")
+    transcript = openai.Audio.transcribe("whisper-1", audio_file)
+    return transcript
 
 @app.route("/ask", methods=['POST'])
 def ask():
