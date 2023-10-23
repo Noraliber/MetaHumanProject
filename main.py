17513ba76edabeaca5f9651c24a03051e932b07b
Noraliber <108892844+Noraliber@users.noreply.github.com>
2023-10-23 14:55:31 +0000

    Update main.py

--- main.py
+++ main.py
@@ -5,21 +5,24 @@ import os
 OPENAI_API_KEY = os.environ['OPENAI_API_KEY']
 openai.api_key = OPENAI_API_KEY
 
+
 app = Flask(__name__)
+
 
 @app.route("/")
 def index():
-  return render_template('index.html')
+    return render_template('index.html')
+
 
 @app.route("/transcribe", methods=['POST'])
 def transcribe():
-  #Transcribe the given audio to text using Whisper.
-  return null
+    audio_file= open("/path/to/file/audio.mp3", "rb")
+
 
 @app.route("/ask", methods=['POST'])
 def ask():
-  #Generate a ChatGPT response from the given conversation
-  return null
+    pass
+
 
 if __name__ == "__main__":
     app.run(host="localhost", port=8000, debug=True)
