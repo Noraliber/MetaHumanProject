e01a9ff22de520c8ed9078cdca1c82f38a02d4aa
Noraliber <108892844+Noraliber@users.noreply.github.com>
2023-10-24 15:10:48 +0000

    Update 3 files

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import openai
 import os
+import uuid
 
 OPENAI_API_KEY = os.environ['OPENAI_API_KEY']
 openai.api_key = OPENAI_API_KEY
@@ -16,9 +17,15 @@ def index():
 
 @app.route("/transcribe", methods=['POST'])
 def transcribe():
-    audio_file= open("/workspace/Enregistrement.m4a", "rb")
+    file = request.files["file"]
+    recording_file = f"{uuid.uuid4()}.wav"
+    recording_path = f"Enregistrements/{recording_file}"
+    os.makedirs(os.path.dirname(recording_path), exist_ok=True)
+    file.save(recording_path)
+    audio_file = open(recording_path, "rb")
     transcript = openai.Audio.transcribe("whisper-1", audio_file)
     return transcript
+
 
 @app.route("/ask", methods=['POST'])
 def ask():
